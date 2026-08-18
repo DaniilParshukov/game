c96a5162c7116271227132c530b92cd8ebc87873
DaniilParshukov <danil.parshukov2004@gmail.com>
2026-08-18 15:41:57 +0700

    debag

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ import { LocalStorageAdapter } from './storage/LocalStorageAdapter.js';
 import { LocalPrices } from './prices/LocalPrices.js';
 
 const storage = new LocalStorageAdapter();
-const prices = null;
+prices = null;
 const gameEngine = new GameEngine(storage, prices);
 
 let gameData = null;
