5200a5a5a41a8db382e28532178e6534aea72be8
DaniilParshukov <danil.parshukov2004@gmail.com>
2026-08-29 19:48:11 +0700

    debag

--- pages/app-shell.js
+++ pages/app-shell.js
@@ -1,5 +1,5 @@
 (function () {
-  const storage = window.getStorage();
+  const storage = window.game.getStorage();
 
   function getPlayerName() {
     if (!storage || typeof storage.loadPlayerName !== 'function') {
